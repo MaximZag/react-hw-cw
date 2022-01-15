63eef655a9064e849ada7a6cb5d98465f2176675
MaximZag <maximus9023@gmail.com>
2022-01-15 22:49:09 +0200

    2.2

--- src/components/Userdetail/Userdetail.js
+++ src/components/Userdetail/Userdetail.js
@@ -4,17 +4,21 @@ import './Userdetails.css'
 
 const Userdetail = ({user, showPosts}) => {
 
+    const {id, name, username, email, address:{street, city}, phone} = user
+
     return (
         <div>
             <div className={'maindet'}>
-                {Object.keys(user).map(userx => {
-                        if (typeof user[userx] !== "object") {
-                            return <div key={userx} className={'userdetails'}>
-                                {user[userx]}
-                            </div>
-                        }
-                    }
-                )}
+                <div className={'userdetails'}>
+                    <div>{id}</div>
+                    <div>{name}</div>
+                    <div>{username}</div>
+                    <div>{email}</div>
+                    <div>{street}</div>
+                    <div>{city}</div>
+                    <div>{phone}</div>
+                </div>
+
                 <button onClick={() => showPosts(user.id)}>Show posts</button>
 
             </div>
