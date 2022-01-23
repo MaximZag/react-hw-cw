ec3e808f8834c68040c5b1a2ca5cf71296558c15
MaximZag <maximus9023@gmail.com>
2022-01-23 17:29:24 +0200

    additional

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link, Outlet} from "react-router-dom";
+
+import css from "./Layout.css";
+
+const Layout = () => {
+    return (
+        <>
+            <div className={css.header}>
+                <Link to={'/episode'}>
+                    <button>Episodes</button>
+                </Link>
+            </div>
+            <div className={css.outlet}>
+                <Outlet/>
+            </div>
+        </>
+    );
+};
+
+export default Layout;
