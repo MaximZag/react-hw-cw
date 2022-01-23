d8f24ce3f8ca0b4d8eefd811df2b200bf54a9bf2
MaximZag <maximus9023@gmail.com>
2022-01-23 17:32:54 +0200

    additional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
-import {EpisodesPage, UserDetails, UserPosts, AlbumsPage, PhotosPage, PostsPage, PostDetails, CommentsPage} from "./pages";
+import {EpisodesPage} from "./pages";
 
 
 function App() {
@@ -9,21 +9,13 @@ function App() {
         <>
             <Routes>
                 <Route path={'/'} element={<Layout/>}>
-                    <Route index element={<Navigate to={'users'}/>}/>
+                    <Route index element={<Navigate to={'episode'}/>}/>
 
-                    <Route path={'users'} element={<EpisodesPage/>}>
-                        <Route path={':id'} element={<UserDetails/>}>
-                            <Route path={'posts'} element={<UserPosts/>}/>
-                        </Route>
+                    <Route path={'episode'} element={<EpisodesPage/>}>
+                        {/*<Route path={':id'} element={<UserDetails/>}>*/}
+                        {/*    <Route path={'posts'} element={<UserPosts/>}/>*/}
+                        {/*</Route>*/}
 
-                        <Route path={':id/albums'} element={<AlbumsPage/>}>
-                            <Route path={':albumId/photos'} element={<PhotosPage/>}/>
-                        </Route>
-                    </Route>
-                    <Route path={'posts'} element={<PostsPage/>}>
-                        <Route path={':id'} element={<PostDetails/>}>
-                            <Route path={'comments'} element={<CommentsPage/>}/>
-                        </Route>
                     </Route>
                 </Route>
             </Routes>
