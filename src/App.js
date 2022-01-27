53d05f07b49977106f8ff15b9140171aba0d1880
MaximZag <maximus9023@gmail.com>
2022-01-27 23:01:02 +0200

    lesson4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import './App.css';
+import {useReducer, createRef} from "react";
+
+const reducer = (state, action) => {
+    if (action.type === 'cat') {
+        state = {...state, cat: action.payload}
+    } else if (action.type === 'dog') {
+        state = {...state, dog: action.payload}
+    }
+    return state
+}
 
 function App() {
-  return (
-    <div>
+    const cat = createRef()
+    const dog = createRef()
 
-    </div>
-  );
+    const sendCat = (e) => {
+        e.preventDefault()
+        dispatch({type: 'cat', payload: cat.current.value})
+    }
+
+    const sendDog = (e) => {
+        e.preventDefault()
+        dispatch({type: 'dog', payload: dog.current.value})
+    }
+
+    const [state, dispatch] = useReducer(reducer, {cat: '', dog: ''});
+
+    return (
+        <div>
+            <form onSubmit={sendCat}>
+                <div><label>Add cat <input type="text" name={'cat'} ref={cat}/></label></div>
+                <button>Save</button>
+            </form>
+            <form onSubmit={sendDog}>
+                <div><label>Add cat <input type="text" name={'dog'} ref={dog}/></label></div>
+                <button>Save</button>
+            </form>
+            {state.cat && <div>
+                {state.cat}
+                <button>Delete</button>
+            </div>}
+            {state.dog && <div>
+                {state.dog}
+                <button>Delete</button>
+            </div>}
+        </div>
+    );
 }
 
 export default App;
